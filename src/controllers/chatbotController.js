ce8dba78afbbd3d73763bf6a7f7e52e2781fc1f5
Kieu Tien Thanh <kieutienthanh207@gmail.com>
2024-07-05 14:39:02 +0700

    fix: add console.log to show the psid

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -45,8 +45,8 @@ const postWebhook = (req, res) => {
 
       // GET the sender PSID
       let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
     });
-
 
     // Returns a '200 OK' response to all requests
     res.status(200).send("EVENT_RECEIVED");
