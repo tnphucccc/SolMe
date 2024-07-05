23e0e5113052c2b7a2f0d0ab6368f678f988ac38
Kieu Tien Thanh <kieutienthanh207@gmail.com>
2024-07-05 15:32:55 +0700

    feat: confirm image sent

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -77,6 +77,37 @@ function handleMessage(sender_psid, received_message) {
     response = {
       text: `Bạn đã nhắn: "${received_message.text}". Giờ cho mình xin ảnh!`,
     };
+  } 
+  else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes",
+                },
+                {
+                  type: "postback",
+                  title: "No!",
+                  payload: "no",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    };
   }
 
   // Sends the response message
