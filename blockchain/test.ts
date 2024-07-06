937cee5e75a3bf27f90498137dc5aa85e123196b
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-07-06 20:31:11 +0700

    Add test for transaction

--- blockchain/test.ts
+++ blockchain/test.ts
@@ -1,4 +1,11 @@
 // write a function to display the balance
-import { displayBalance } from "./main";
+import { displayBalance, makeTransaction } from "./main";
 
 displayBalance("CoT2U8WaGhRytk8knRkUxwd2K41MhoCK31m6iJ4nDU59");
+makeTransaction(
+  "cd00851481ebef25f2c279c5d7b7a600b6b573c4161ea79982ba006019d76f7ab88bb9c59da0f2eaf8c2a6492f9369b8ae1f62b96b224fe8d2d5c6974ab909cf",
+  "DRPbFNor7BWxQdBANCU55MbJXefDDKqGPEDvYjd4HDqG",
+  2.3
+);
+
+//  Make a transaction (replace with valid secret key and destination wallet address)
